00705828dce65915fec5b08db8bbd10c94a5a631
Rennan Macarini <rennangmacarini@gmail.com>
2023-05-13 15:26:58 -0300

    feat: progress movie pages `:zap: `

---
+++ src/pages/Films/index.jsx
@@ -0,0 +1,13 @@
+import { CarouselNote } from "../../components/carouselNote";
+import { NewFilms } from "../../components/newFilms";
+
+function Films() {
+  return (
+    <> 
+    <NewFilms/>
+    <CarouselNote/>
+    </>
+  );
+}
+
+export default Films;
