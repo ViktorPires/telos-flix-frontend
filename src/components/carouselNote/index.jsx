6e314f8828ecd34b89557c9f061c9adb883c6ba1
Rennan Macarini <rennangmacarini@gmail.com>
2023-05-15 15:20:04 -0300

    creating css in carousel components

--- src/components/carouselNote/index.jsx
+++ src/components/carouselNote/index.jsx
@@ -1,20 +1,72 @@
-import { Star, StarBorderOutlined } from "@mui/icons-material";
+import { Star, StarBorderOutlined, StarBorderPurple500Outlined } from "@mui/icons-material";
 import SecondaryGradientButton from "../secondaryGrandientButton";
 import LinearProgress from '@mui/material/LinearProgress';
-import { Stack } from "@mui/material";
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+
+import "./index.css";
+import { useState } from "react";
+
 
 export function CarouselNote() {
+  const [filmsModal, setFilmsModal] = useState(false);
+
+  const handleAssessModal = (event) => {
+    setFilmsModal( !filmsModal)
+  };
+
+ 
+
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout
+        let mouseOver = false
+        function clearNextTimeout() {
+          clearTimeout(timeout)
+        }
+        function nextTimeout() {
+          clearTimeout(timeout)
+          if (mouseOver) return
+          timeout = setTimeout(() => {
+            slider.next()
+          }, 8000)
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+          })
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+          })
+          nextTimeout()
+        })
+        slider.on("dragStarted", clearNextTimeout)
+        slider.on("animationEnded", nextTimeout)
+        slider.on("updated", nextTimeout)
+      },
+    ]
+  )
+
   return (
     <>
-      <div style={{ width: "1222px", margin: "auto" }}>
-        <SecondaryGradientButton
-          icon={<StarBorderOutlined />}
-          text="Avaliar"
-        />
-
+      <div style={{ width: "1222px", margin: "auto", marginBottom: "5rem" }}>
         <div style={{ display: "flex", alignItems: "center", gap: "18px" }}>
           <Star />
           <h3>Se liga nessas avaliações</h3>
+
+          <div style={{marginLeft: "120px", marginTop: "-2rem"}}>
+            <SecondaryGradientButton
+              onClick={filmsModal}
+              icon={<StarBorderOutlined />}
+              text="Avaliar"
+            />
+          </div>
         </div>
 
         <div style={{ display: "flex", alignItems: "center", gap: "33px" }}>
@@ -30,7 +82,6 @@ export function CarouselNote() {
               <h6>2</h6>
               <h6>1</h6>
             </div>
-
 
             <div style={{ display: "flex", alignItems: "center", flexDirection: "column", gap: "17px" }}>
               <Star sx={{ width: "15px" }} />
@@ -111,10 +162,95 @@ export function CarouselNote() {
                 }}
               /></h6>
             </div>
+
+            <div ref={sliderRef} className="keen-slider">
+              <div className="keen-slider__slide number-slide1" style={{ display: "flex", gap: "20px" }}>
+                <div className="carouselCard" style={{ textAlign: "start" }}>
+                  <h1>Paula Rust</h1>
+                  <p style={{ width: "300px" }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                  <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                    <span>4.5</span>
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                  </div>
+                </div>
+
+                <div className="carouselCard" style={{ textAlign: "start" }}>
+                  <h1>Paula Rust</h1>
+                  <p style={{ width: "300px" }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                  <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                    <span>4.5</span>
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                  </div>
+                </div>
+              </div>
+
+              <div className="keen-slider__slide number-slide1" style={{ display: "flex", gap: "20px" }}>
+                <div className="carouselCard" style={{ textAlign: "start" }}>
+                  <h1>Paula Rust</h1>
+                  <p style={{ width: "300px" }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                  <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                    <span>4.5</span>
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                  </div>
+                </div>
+
+                <div className="carouselCard" style={{ textAlign: "start" }}>
+                  <h1>Paula Rust</h1>
+                  <p style={{ width: "300px" }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                  <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                    <span>4.5</span>
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                  </div>
+                </div>
+              </div>
+
+              <div className="keen-slider__slide number-slide1" style={{ display: "flex", gap: "20px" }}>
+                <div className="carouselCard" style={{ textAlign: "start" }}>
+                  <h1>Paula Rust</h1>
+                  <p style={{ width: "300px" }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                  <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                    <span>4.5</span>
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                  </div>
+                </div>
+
+                <div className="carouselCard" style={{ textAlign: "start" }}>
+                  <h1>Paula Rust</h1>
+                  <p style={{ width: "300px" }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                  <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+                    <span>4.5</span>
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                    <StarBorderPurple500Outlined />
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
-
-
       </div>
 
 
