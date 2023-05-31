a17cc11584e94bfdd8ffdcdbbeed68ad771e3efe
Rennan Macarini <rennangmacarini@gmail.com>
2023-05-31 16:05:17 -0300

    feat: adjusting video

--- src/pages/video/index.jsx
+++ src/pages/video/index.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
-import { CardGiftcardOutlined } from "@mui/icons-material";
 import { Link, useParams } from "react-router-dom";
 import { MovieContext } from "../../contexts/MovieContext";
+import { AuthenticateContext } from "../../contexts/AuthenticateContext";
 
 function Video() {
   const [movies] = useContext(MovieContext);
   const { id } = useParams(); 
+  const { isAuthenticated } = useContext(AuthenticateContext);
 
   const movie = movies.find((movie) => movie._id === id);
 
@@ -18,22 +19,30 @@ function Video() {
   return (
     <div className="videoPage">
       <div className="videoGrid">
-        {videoId ? (
-          <div className="videoCard">
-            <iframe
-              width="300"
-              height="220"
-              src={`https://www.youtube.com/embed/${videoId}`}
-              title="video"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-            <Link to={`/films/${movie._id}`}>
-              <div className="videoOverlay"></div>
-            </Link>
-          </div>
+        {isAuthenticated ? (
+          videoId ? (
+            <div className="videoCard">
+              <iframe
+                width="300"
+                height="220"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="video"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+              <Link to={`/films/${movie._id}`}>
+                <div className="videoOverlay"></div>
+              </Link>
+            </div>
+          ) : (
+            <div className="videoPlaceholder"></div>
+          )
         ) : (
-          <div className="videoPlaceholder"></div>
+          <img
+            src={movie.image}
+            alt=""
+            style={{ height: "300px", width: "400px", objectFit: "cover" }}
+          />
         )}
         <h1 style={{ fontSize: "16px" }}>{movie.title}</h1>
       </div>
