3735210389ce6937797e1e6a84be972eb4a758e3
Viktor Oleksiuk <viktor@Viktors-iMac.local>
2024-03-13 23:17:01 -0300

    feat(cardsfilms): added loading screen on cardsFilms page

--- src/pages/cardsFilms/index.jsx
+++ src/pages/cardsFilms/index.jsx
@@ -1,74 +1,87 @@
-import React, {
-  useState, useContext, useEffect
-} from 'react';
-import './index.css';
-import { ArrowForward } from '@mui/icons-material';
-import Header from '../../components/header';
-import { MovieContext } from '../../contexts/MovieContext';
-import { Link, useParams } from 'react-router-dom';
-import { Zoom } from 'react-reveal';
+import React, { useState, useContext, useEffect } from "react";
+import "./index.css";
+import { ArrowForward } from "@mui/icons-material";
+import Header from "../../components/header";
+import { MovieContext } from "../../contexts/MovieContext";
+import { Link, useParams } from "react-router-dom";
+import { Zoom } from "react-reveal";
+import Loading from "../../components/loading";
 
 export default function CardsFilms() {
   const { genre } = useParams();
 
-  const { search, movieGenres } = useContext(MovieContext);
-  const [searchTerm, setSearchTerm] = useState('');
+  const { search, movieGenres, isLoading, isMovieGenresLoading } =
+    useContext(MovieContext);
+  const [searchTerm, setSearchTerm] = useState("");
   const [selectedCategory, setSelectedCategory] = useState(genre);
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function handleMovies() {
-      const movies = await search(searchTerm, selectedCategory)
-      setMovies(movies)
+      const movies = await search(searchTerm, selectedCategory);
+      setMovies(movies);
     }
-    handleMovies()
-  }, [searchTerm, selectedCategory])
+    handleMovies();
+  }, [searchTerm, selectedCategory]);
 
-  return (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <>
       <Header />
-      <div data-testid="cards-films-component" className='cardsFilms-container'>
-        <div className='cardsFilms-content'>
-          <div className='input-content-cardsFilms'>
-            <form action="#" method="get" autoComplete='off'>
+      <div data-testid="cards-films-component" className="cardsFilms-container">
+        <div className="cardsFilms-content">
+          <div className="input-content-cardsFilms">
+            <form action="#" method="get" autoComplete="off">
               <input
-                className='input-name-cardsFilm'
+                className="input-name-cardsFilm"
                 type="text"
-                name='nameFilm'
-                id='inameFilm'
-                placeholder='Title'
+                name="nameFilm"
+                id="inameFilm"
+                placeholder="Title"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
               <select
-                className='select-category-cardsFilm'
+                className="select-category-cardsFilm"
                 name="category"
                 id="icategory"
-                placeholder='Category'
+                placeholder="Category"
                 value={selectedCategory}
                 onChange={(e) => setSelectedCategory(e.target.value)}
               >
-                <option value="" disabled>Category <ArrowForward /></option>
+                <option value="" disabled>
+                  Category <ArrowForward />
+                </option>
                 {movieGenres.map((genre) => (
-                  <option key={genre} value={genre}>{genre}</option>
+                  <option key={genre} value={genre}>
+                    {genre}
+                  </option>
                 ))}
               </select>
             </form>
           </div>
-          <div className='grid-cardsFilms'>
-
-            {movies.map(movie => (
+          <div className="grid-cardsFilms">
+            {movies.map((movie) => (
               <Zoom top distance="30%" duraction={1500}>
-                <Link to={`/films/${movie._id}`} key={movie._id} style={{ textDecoration: "none" }}>
+                <Link
+                  to={`/films/${movie._id}`}
+                  key={movie._id}
+                  style={{ textDecoration: "none" }}
+                >
                   <div className="miniVideoCard">
-
                     <img
-                      style={{ marginTop: "1rem", objectFit: "cover", height: "100%", borderRadius: 12 }}
+                      style={{
+                        marginTop: "1rem",
+                        objectFit: "cover",
+                        height: "100%",
+                        borderRadius: 12,
+                      }}
                       src={movie.image}
                       alt={movie.title}
                     />
                     <div style={{ margin: "0 auto" }}>
-                      <h1 style={{ fontSize: "14px", }}>{movie.title}</h1>
+                      <h1 style={{ fontSize: "14px" }}>{movie.title}</h1>
                     </div>
                   </div>
                 </Link>
@@ -76,7 +89,7 @@ export default function CardsFilms() {
             ))}
           </div>
         </div>
-      </div >
+      </div>
     </>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import Trending from "../../components/trending";
 import EnjoyForFree from "../../components/enjoyForFree";
-import './index.css'
+import "./index.css";
 import DontKnowWhatToWatch from "../../components/dontKnowWhatToWatch";
 import Header from "../../components/header";
 import HomeCarousel from "../../components/HomeCarousel";
@@ -10,15 +10,19 @@ import { MovieContext } from "../../contexts/MovieContext";
 
 function Home() {
   const { isLoading } = useContext(MovieContext);
-  
-  if (isLoading) {
-    return <Loading />
-  }
 
-  return (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div data-testid="home-component">
       <Header />
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <HomeCarousel />
         <div className="sectionsGrid">
           <Trending />
